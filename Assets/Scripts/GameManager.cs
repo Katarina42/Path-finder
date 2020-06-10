229d5df2ac9c0c16768779bbba75bcf6729e03b2
Katarina42 <katarinarankovic42@gmail.com>
2020-06-10 23:24:42 +0200

    Game setup

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager instance;
+    public static GameManager Instance
+    {
+        get { return instance; }     
+    }
+
+    #region setup
+
+    private const float TILE_SIZE = 100;
+    public GameObject gridTilePrefab;
+    public GameObject playerPrefab;
+    public GameObject enemyPrefab;
+
+    public Point grid;
+    public Point player;
+    public Point enemy;
+
+    private GameObject gridParent;
+
+    #endregion
+
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+            DestroyImmediate(this.gameObject);
+
+        instance = this;
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    public void InitializeGame()
+    {
+        StartCoroutine(Initializing());
+    }
+
+    private void SetGrid()
+    {
+        //seting grid
+        RectTransform rect = gridParent.GetComponent<RectTransform>();
+        rect.sizeDelta = new Vector2(grid.x * TILE_SIZE, grid.y * TILE_SIZE);
+
+        //seting tiles
+        for (int i = 0; i < grid.x * grid.y; i++)
+        {
+            Instantiate(gridTilePrefab, gridParent.transform);
+        }
+    }
+
+    IEnumerator Initializing()
+    {
+        yield return new WaitUntil(() => GameObject.FindGameObjectWithTag("Grid") != null);
+        gridParent = GameObject.FindGameObjectWithTag("Grid");
+
+        SetGrid();
+        SetPlayer();
+        SetEnemy();
+    }
+
+    private void SetPlayer()
+    {
+        Instantiate(playerPrefab, gridParent.transform);
+
+    }
+
+    private void SetEnemy()
+    {
+        Instantiate(enemyPrefab, gridParent.transform);
+
+    }
+
+}
+
+public struct Point
+{
+    public int x;
+    public int y;
+}
--- Assets/Scripts/UiManager.cs
+++ Assets/Scripts/UiManager.cs
@@ -1,13 +1,40 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class UiManager : MonoBehaviour
 {
-   
+
+    [Header("Grid")]
+    public TMP_InputField gridXText;
+    public TMP_InputField gridYText;
+
+    [Header("Player")]
+    public TMP_InputField playerXText;
+    public TMP_InputField playerYText;
+
+    [Header("Enemy")]
+    public TMP_InputField enemyXText;
+    public TMP_InputField enemyYText;
+
     public void OnPlay()
     {
+        ParseTextInput();
         SceneManager.LoadScene("Grid");
+        GameManager.Instance.InitializeGame();
+    }
+
+    private void ParseTextInput()
+    {
+        int.TryParse(gridXText.text, out GameManager.Instance.grid.x);
+        int.TryParse(gridYText.text, out GameManager.Instance.grid.y);
+        int.TryParse(playerXText.text, out GameManager.Instance.player.x);
+        int.TryParse(playerYText.text, out GameManager.Instance.player.y);
+        int.TryParse(enemyXText.text, out GameManager.Instance.enemy.x);
+        int.TryParse(enemyYText.text, out GameManager.Instance.enemy.y);
     }
 }
