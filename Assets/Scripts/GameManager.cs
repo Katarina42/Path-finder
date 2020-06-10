bd66db1e4ef8ef82dc348ffd526d3c62a73bfe10
Katarina42 <katarinarankovic42@gmail.com>
2020-06-11 01:32:18 +0200

    Path Finiding

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -10,7 +10,7 @@ public class GameManager : MonoBehaviour
         get { return instance; }     
     }
 
-    #region setup
+    #region Setup data
 
     private const float TILE_SIZE = 100;
     public GameObject gridTilePrefab;
@@ -22,8 +22,11 @@ public class GameManager : MonoBehaviour
     public Point enemy;
 
     private GameObject gridParent;
+    private PathFinder pathFinder;
 
     #endregion
+
+    #region Unity
 
     private void Awake()
     {
@@ -33,24 +36,19 @@ public class GameManager : MonoBehaviour
         instance = this;
 
         DontDestroyOnLoad(this.gameObject);
+
+        grid = new Point(10,10);
+        enemy = new Point(9, 4);
+        player = new Point(0, 4);
     }
+
+    #endregion
+
+    #region Game initialization
 
     public void InitializeGame()
     {
         StartCoroutine(Initializing());
-    }
-
-    private void SetGrid()
-    {
-        //seting grid
-        RectTransform rect = gridParent.GetComponent<RectTransform>();
-        rect.sizeDelta = new Vector2(grid.x * TILE_SIZE, grid.y * TILE_SIZE);
-
-        //seting tiles
-        for (int i = 0; i < grid.x * grid.y; i++)
-        {
-            Instantiate(gridTilePrefab, gridParent.transform);
-        }
     }
 
     IEnumerator Initializing()
@@ -63,22 +61,161 @@ public class GameManager : MonoBehaviour
         SetEnemy();
     }
 
+    private void SetGrid()
+    {
+        //seting grid
+        RectTransform rect = gridParent.GetComponent<RectTransform>();
+        rect.sizeDelta = new Vector2(grid.x * TILE_SIZE, grid.y * TILE_SIZE);
+        pathFinder = new PathFinder(grid.x, grid.y);
+
+        //seting tiles
+        for (int i = 0; i < grid.x; i++)
+        {
+            for (int j = 0; j < grid.y; j++)
+            {
+                GameObject tile = Instantiate(gridTilePrefab, gridParent.transform);
+                pathFinder.gridMatrix[i, j].tile = tile;
+                pathFinder.gridMatrix[i, j].visited = false;
+            }
+        }
+    }
+
     private void SetPlayer()
     {
-        Instantiate(playerPrefab, gridParent.transform);
+        Instantiate(playerPrefab, pathFinder.gridMatrix[player.x,player.y].tile.transform);
 
     }
 
     private void SetEnemy()
     {
-        Instantiate(enemyPrefab, gridParent.transform);
+        Instantiate(enemyPrefab, pathFinder.gridMatrix[enemy.x, enemy.y].tile.transform);
+    }
 
+    #endregion
+}
+
+public class PathFinder
+{
+    public Grid[,] gridMatrix;
+    private Queue<Point> que;
+    private int length;
+
+    public PathFinder(int m,int n)
+    {
+        gridMatrix = new Grid[m,n];
+        que = new Queue<Point>();
+
+        Point enemyPoint = FindShortestPath();
+        if (enemyPoint.x == -1)
+            Debug.Log("There is no path");
+        else
+        {
+            int counter = PrintPath(enemyPoint);
+
+            Debug.Log("Path length is " + counter);
+        }
+
+    }
+
+    private int PrintPath(Point point)
+    {
+        if (gridMatrix[point.x,point.y].parent != null)
+        {
+            length++;
+            Debug.Log("" + point.x + "," + point.y);
+            PrintPath(gridMatrix[point.x, point.y].parent);
+        }
+
+        return length;
+    }
+
+    private Point FindShortestPath()
+    {
+        int x = GameManager.Instance.player.x;
+        int y = GameManager.Instance.player.y;
+
+        Point startPoint = new Point (x,y);
+        gridMatrix[x, y].parent = null;
+        que.Enqueue(startPoint);
+        gridMatrix[x,y].visited = true;
+
+        while(que.Count>0)
+        {
+            Point currentPoint = que.Dequeue();
+            x = currentPoint.x;
+            y = currentPoint.y;
+
+            //Debug.Log("Current " + x + ":" + y);
+
+            if (currentPoint.x == GameManager.Instance.enemy.x && currentPoint.y == GameManager.Instance.enemy.y)
+            {
+                return currentPoint;
+            }
+
+            //left
+            if(ValidPoint(x-1,y) && !gridMatrix[x-1, y].visited)
+            {
+                gridMatrix[x - 1, y].visited = true;
+                gridMatrix[x-1, y].parent = currentPoint;
+                Point point = new Point(x - 1, y);
+                que.Enqueue(point);
+            }
+
+            //right
+            if (ValidPoint(x+1, y) && !gridMatrix[x+1, y].visited)
+            {
+                gridMatrix[x + 1, y].visited = true;
+                gridMatrix[x+1, y].parent = currentPoint;
+                Point point = new Point(x + 1, y);
+                que.Enqueue(point);
+            }
+            //up
+            if (ValidPoint(x, y+1) && !gridMatrix[x, y +1].visited)
+            {
+                gridMatrix[x, y+1].visited = true;
+                gridMatrix[x, y+1].parent = currentPoint;
+                Point point = new Point(x, y + 1);
+                que.Enqueue(point);
+            }
+            //down
+            if (ValidPoint(x, y-1) && !gridMatrix[x, y - 1].visited)
+            {
+                gridMatrix[x , y-1].visited = true;
+                gridMatrix[x, y-1].parent = currentPoint;
+                Point point = new Point(x, y - 1);
+                que.Enqueue(point);
+            }
+
+        }
+
+        return new Point(-1, -1);
+    }
+
+    private bool ValidPoint(int x,int y)
+    {
+        if (x >= 0 && x < GameManager.Instance.grid.x && y >= 0 && y < GameManager.Instance.grid.y)
+            return true;
+
+        return false;
     }
 
 }
 
-public struct Point
+public struct Grid
+{
+    public GameObject tile;
+    public bool visited;
+    public Point parent;
+}
+
+public class Point
 {
     public int x;
     public int y;
+
+    public Point(int x, int y )
+    {
+        this.x = x;
+        this.y = y;
+    }
 }
